4ec723ff7af52895f69084d61d59d6e911e86229
Adnan <adnanm2002@outlook.com>
2023-12-30 10:05:20 +0300

    example of drag and drop

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ViewChildren, ElementRef } from '@angular/core';
+import { GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements AfterViewInit {
+  @ViewChild('rectangle') rectangle: ElementRef | any;
 
-  constructor() {}
+  type:string= '';
+  currentX:number = 0;
+  
+
+  
+
+  constructor(private gest:GestureController) {}
+
+  ngAfterViewInit(): void {
+    this.updateGesture();
+    
+  }
+
+  updateGesture(){
+    const drag = this.gest.create({
+      el:this.rectangle.nativeElement,
+      threshold:1,
+      gestureName:'drag',
+      onMove:ev => {
+          this.type = ev.type;
+          this.currentX = ev.currentX;
+          console.log(ev.type, ev.currentX);
+      },
+    });
+    drag.enable();
+  }
 
 }
